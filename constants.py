56e4aa9cf5dcb9ee7868d2f71b733b4f05b64d7b
vlatan <33163699+vlatan@users.noreply.github.com>
2024-05-02 01:00:56 +0200

    Remove files

--- constants.py
+++
@@ -1,3 +0,0 @@
-# https://data.worldbank.org/country/north-macedonia?view=chart
-
-API_ROOT = "https://api.worldbank.org/v2/country/mk/indicator"
--- css.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-st.markdown(
-    "<style>div[data-baseweb='select']>div:hover{cursor:pointer}</style>",
-    unsafe_allow_html=True,
-)
